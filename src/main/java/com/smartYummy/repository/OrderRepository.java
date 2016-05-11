9da1ed88b51252fb981977b80b4caf32ac0cc4df
chenglongwei <weichenglong@gmail.com>
2016-05-10 20:11:57 -0700

    update order

--- src/main/java/com/smartYummy/repository/OrderRepository.java
+++ src/main/java/com/smartYummy/repository/OrderRepository.java
@@ -2,8 +2,10 @@ package com.smartYummy.repository;
 
 import com.smartYummy.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 /**
