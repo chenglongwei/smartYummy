b0c8db78e65d65b2fc59217875512540d471026c
chenglongwei <weichenglong@gmail.com>
2016-05-10 17:05:45 -0700

    update shopping cart api

--- src/main/java/com/smartYummy/controller/ShoppingCartController.java
+++ src/main/java/com/smartYummy/controller/ShoppingCartController.java
@@ -7,11 +7,14 @@ import com.smartYummy.service.ItemService;
 import com.smartYummy.service.OrderService;
 import com.smartYummy.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -29,6 +32,7 @@ public class ShoppingCartController {
     private ItemService itemService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     void addItem(@RequestParam("item_id") long item_id,
                  @RequestParam("quantity") int quantity,
                  Authentication authentication) {
@@ -38,6 +42,7 @@ public class ShoppingCartController {
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     void updateItem(@RequestParam("item_id") long item_id,
                     @RequestParam("quantity") int quantity,
                     Authentication authentication) {
@@ -47,6 +52,7 @@ public class ShoppingCartController {
     }
 
     @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     void removeItem(@RequestParam("item_id") long item_id,
                     Authentication authentication) {
         OrderItem orderItem = getOrderItem(authentication, item_id);
@@ -54,11 +60,13 @@ public class ShoppingCartController {
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    List<OrderItem> getItems() {
-        return shoppingCartService.getOrderItems();
+    String getItems(Model model) {
+        model.addAttribute("order_items", shoppingCartService.getOrderItems());
+        return "shopping/list";
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     void save(Authentication authentication) {
         Order order = new Order();
         // insert user bean
