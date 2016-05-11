603971478cccbb965dfd972e362a5fb12396b041
tao <lijun.tao@yahoo.com>
2016-05-11 10:32:36 -0700

    clean code in item list and cotroller.js

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,45 +1,14 @@
 /**
- * Created by Le on 1/11/2016.
+ * Created by Lijun on 5/11/2016.
  */
 
 var cartApp = angular.module ("smartYummy.app", []);
 
 cartApp.controller("cartCtrl", function ($scope, $http){
 
-    //$scope.refreshCart = function () {
-    //    $http.get('/eMusicStore/rest/cart/'+$scope.cartId).success(function (data) {
-    //       $scope.cart=data;
-    //    });
-    //};
-    //
-    //$scope.clearCart = function () {
-    //    $http.delete('/eMusicStore/rest/cart/'+$scope.cartId).success($scope.refreshCart());
-    //};
-    //
-    //$scope.initCartId = function (cartId) {
-    //    $scope.cartId = cartId;
-    //    $scope.refreshCart(cartId);
-    //};
-
     $scope.addToCart = function (itemId) {
         $http.post('/shopping/add?item_id='+ itemId + '&quantity=1').success(function () {
-            alert("Product successfully added to the cart!")
+            alert("Item successfully added to the cart!")
         });
     };
-
-    //$scope.removeFromCart = function (productId) {
-    //    $http.put('/eMusicStore/rest/cart/remove/'+productId).success(function (data) {
-    //        $scope.refreshCart();
-    //    });
-    //};
-    //
-    //$scope.calGrandTotal = function () {
-    //    var grandTotal=0;
-    //
-    //    for (var i=0; i<$scope.cart.cartItems.length; i++) {
-    //        grandTotal+=$scope.cart.cartItems[i].totalPrice;
-    //    }
-    //
-    //    return grandTotal;
-    //};
 });
